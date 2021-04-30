8fe9e9ada4dabecef471f68fbae500206f0d7dc9
MaxSomewhere <maxpower199510@gmail.com>
2021-04-30 11:13:49 +0300

    30.04

--- cypress/integration/Auth.js
+++ cypress/integration/Auth.js
@@ -5,10 +5,16 @@ describe ("Log in", () => {
         cy.viewport(1920, 1080)
     })
 
- 
+let selectAuthContract = '[rel="user"]';
+let selectAuthEmail = '[rel="email"]';
+
+
     it ("Валидация полей авторизации",() => {
         cy.fixture('lktests').then(lk => {
             cy.visit(lk.baseUrl)
+
+            //Выбор метода авторизации "№ Договора + пароль"
+            loginForm.selectAuthInput(selectAuthContract)
 
             //Авторизация с невалидным № Договора
             loginForm.userAuthContract(lk.invalidAkkNum , lk.correctAkkPass)
--- cypress/pageobject/loginForm.js
+++ cypress/pageobject/loginForm.js
@@ -1,37 +1,35 @@
 let inputSelectAuth = '.select-styled';
-let selectAuthContract = '[rel="user"]';
-let selectAuthEmail = '[rel="email"]';
 let inputLogin = '#userid';
 let inputPassword = '#userpassword';
 let buttonSubmit = 'button[class="btn shadow login-btn"]';
 
 export class AuthLoginform {
 
+    //Выбор способа авторизации
+    selectAuthInput (selectInput) {
+        cy.get(inputSelectAuth).click()
+        cy.get(selectInput).click({ force: true })
+    }
+
     //Авторизация при помощи номера договора
     userAuthContract(login, password) {
-        cy.get(inputSelectAuth).click()
-        cy.get(selectAuthContract).click({ force: true })
-        cy.get(inputLogin).type(login)
-        cy.get(inputPassword).type(password)
+        cy.get(inputLogin).clear().type(login)
+        cy.get(inputPassword).clear().type(password)
         cy.get(buttonSubmit).click()
     }
 
     //Авторизация при помощи логина
     userAuthLogin(login, password) {
-        // cy.get(inputSelectAuth).click()
-        // cy.get(selectAuthContract).click({ force: true })
-        cy.get(inputLogin).type(login)
-        cy.get(inputPassword).type(password)
+        cy.get(inputLogin).clear().type(login)
+        cy.get(inputPassword).clear().type(password)
         cy.get(buttonSubmit).click()
     }
 
 
     //Авторизация при помощи Email
     userAuthEmail(login, password) {
-        cy.get(inputSelectAuth).click()
-        cy.get(selectAuthEmail).click({ force: true })
-        cy.get(inputLogin).type(login)
-        cy.get(inputPassword).type(password)
+        cy.get(inputLogin).clear().type(login)
+        cy.get(inputPassword).clear().type(password)
         cy.get(buttonSubmit).click()
     }
 }
